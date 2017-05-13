3b98bd58e9dee67d53fe05c04c6cd1c9934ae17f
flyerooo <wxl.pure@gmail.com>
2017-05-13 15:56:40 +0800

    Initial commit

---
+++ 3.1_wiki_spider.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import random
+import re
+
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
+    bsObj = BeautifulSoup(html, 'lxml')
+    return bsObj.find("div", {"id":"bodyContent"}).findAll("a",href=re.compile("^(/wiki/)((?!:).)*$"))
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
