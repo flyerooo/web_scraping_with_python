44e33aa92feefd4a894fc42968ebea0bf5ecadb8
flyerooo <wxl.pure@gmail.com>
2017-05-15 11:32:07 +0800

    add getCountry()

--- 4.8_wikiHistoryIPs.py
+++ 4.8_wikiHistoryIPs.py
@@ -2,12 +2,15 @@
 # coding:utf-8
 
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import datetime
 import random
 import re
+import json
 
 random.seed(datetime.datetime.now())
+
 def getLinks(articleUrl):
     html = urlopen("http://en.wikipedia.org" + articleUrl)
     bsObj = BeautifulSoup(html, "lxml")
@@ -26,8 +29,16 @@ def getHistoryIPs(pageUrl):
     ipAddresses = bsObj.findAll("a", {"class": "mw-anonuserlink"})
     addressList = set()
     for ipAddress in ipAddresses:
-        addressList.add(ipAddress.get_text)
+        addressList.add(ipAddress.get_text())
     return addressList
+
+def getCountry(ipAddress):
+    try:
+        response = urlopen("http://freegeoip.net/json/" + ipAddress).read().decode("utf-8")
+    except HTTPError:
+        return None
+    responseJson = json.loads(response)
+    return responseJson.get("country_code")
 
 links = getLinks("/wiki/Python_(programming_language)")
 
@@ -36,6 +47,8 @@ while(len(links) > 0):
         print("-----------------------")
         historyIPs = getHistoryIPs(link.attrs["href"])
         for historyIP in historyIPs:
-            print(historyIP)
+            country = getCountry(historyIP)
+            if country is not None:
+                print(historyIP + " is from " + country)
     newLink = links[random.randint(0, len(links)-1)].attrs["href"]
     links = getLinks(newLink)
